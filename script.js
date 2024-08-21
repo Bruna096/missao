d7003bea3b3f232c04e83681cdce177733a56178
Brunaa <108538735+Bruna096@users.noreply.github.com>
2024-08-21 12:36:55 -0300

    s

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-perguntas");
+const caixaResultado = document.querySelector(".caixa-perguntas");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
